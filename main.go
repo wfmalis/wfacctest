605130c9900d7de2bacd67622ab93713eb175201
JuiChih <wfmalis@gmail.com>
2017-07-28 14:50:47 +0800

    測試UserID

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(  "Line_ID:" + message.ID)).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(  "Line_ID:" + UserID)).Do(); err != nil {
 					log.Print(err)
 				}
 			}
